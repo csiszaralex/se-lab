a1ec59eef6fa40a0fe73dcb75c5956671193596b
csiszaralex <csiszaralex@gmail.com>
2023-09-20 14:38:34 +0200

    :sprekles: Adds nonused comments for tests

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -109,8 +109,7 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
 
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+        // Ez egy teszt komment :)
 
         break;
     }
